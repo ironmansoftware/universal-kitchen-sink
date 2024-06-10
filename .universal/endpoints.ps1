aefc0b49b00c192679f447fdeed6db5db1b4581c
admin <admin>
2024-06-10 13:09:37 -0500

    Basic Endpoints.

---
+++ .universal/endpoints.ps1
@@ -0,0 +1,9 @@
+﻿New-PSUEndpoint -Url "/api/response/string" -Description "An endpoint that returns a string as a response." -Method @('GET') -Endpoint {
+"Hello, world!"
+} 
+New-PSUEndpoint -Url "/api/response/object" -Description "An endpoint that returns an object." -Method @('GET') -Endpoint {
+ @{
+    Name = "Adam"
+    Title = "Programmer"
+ }
+} -ErrorAction "SilentlyContinue"
