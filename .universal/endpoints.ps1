dd2ed33e501fe21b6873f231b7b380a95a997cc3
admin <admin>
2024-06-10 17:10:37 -0500

    Endpoints.

--- .universal/endpoints.ps1
+++ .universal/endpoints.ps1
@@ -2,8 +2,23 @@
 "Hello, world!"
 } 
 New-PSUEndpoint -Url "/api/response/object" -Description "An endpoint that returns an object." -Method @('GET') -Endpoint {
- @{
+@{
     Name = "Adam"
     Title = "Programmer"
  }
-} -ErrorAction "SilentlyContinue"
+} -ErrorAction "SilentlyContinue" 
+New-PSUEndpoint -Url "/api/response/statuscode" -Description "An endpoint that returns a non-200 status code. " -Method @('GET') -Endpoint {
+# Enter your script to process requests.
+} -ErrorAction "SilentlyContinue" 
+New-PSUEndpoint -Url "/api/request/headers" -Description "An endpoint that processes a request's headers." -Method @('GET') -Endpoint {
+# Enter your script to process requests.
+} -ErrorAction "SilentlyContinue" 
+New-PSUEndpoint -Url "/api/request/querystring" -Description "An endpoint that uses the request's query string. " -Method @('GET') -Endpoint {
+# Enter your script to process requests.
+} -ErrorAction "SilentlyContinue" 
+New-PSUEndpoint -Url "/api/request/body" -Description "An endpoint that uses the request's body." -Method @('GET') -Endpoint {
+# Enter your script to process requests.
+} -ErrorAction "SilentlyContinue" 
+New-PSUEndpoint -Url "/api/request/parameters" -Description "An endpoint that uses a param block." -Method @('GET') -Endpoint {
+# Enter your script to process requests.
+} -ErrorAction "SilentlyContinue" 
