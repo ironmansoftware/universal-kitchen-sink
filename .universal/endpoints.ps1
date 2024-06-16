74e6b645d9581a2a2c79647a870c4f10d466f75d
admin <admin>
2024-06-16 09:31:26 -0500

    API Updates.

--- .universal/endpoints.ps1
+++ .universal/endpoints.ps1
@@ -1,24 +1,26 @@
 ﻿New-PSUEndpoint -Url "/api/response/string" -Description "An endpoint that returns a string as a response." -Method @('GET') -Endpoint {
-"Hello, world!"
+"Hello, world!!"
 } 
 New-PSUEndpoint -Url "/api/response/object" -Description "An endpoint that returns an object." -Method @('GET') -Endpoint {
 @{
     Name = "Adam"
     Title = "Programmer"
  }
-} -ErrorAction "SilentlyContinue" 
+} 
 New-PSUEndpoint -Url "/api/response/statuscode" -Description "An endpoint that returns a non-200 status code. " -Method @('GET') -Endpoint {
-# Enter your script to process requests.
-} -ErrorAction "SilentlyContinue" 
+New-PSUApiResponse -StatusCode 418
+} 
 New-PSUEndpoint -Url "/api/request/headers" -Description "An endpoint that processes a request's headers." -Method @('GET') -Endpoint {
-# Enter your script to process requests.
-} -ErrorAction "SilentlyContinue" 
+$Headers["x-test-header"]
+} 
 New-PSUEndpoint -Url "/api/request/querystring" -Description "An endpoint that uses the request's query string. " -Method @('GET') -Endpoint {
-# Enter your script to process requests.
-} -ErrorAction "SilentlyContinue" 
+param($QueryString)
+
+$QueryString
+} 
 New-PSUEndpoint -Url "/api/request/body" -Description "An endpoint that uses the request's body." -Method @('GET') -Endpoint {
-# Enter your script to process requests.
-} -ErrorAction "SilentlyContinue" 
+param($BodyProperty)
+} 
 New-PSUEndpoint -Url "/api/request/parameters" -Description "An endpoint that uses a param block." -Method @('GET') -Endpoint {
 # Enter your script to process requests.
-} -ErrorAction "SilentlyContinue" 
+}
